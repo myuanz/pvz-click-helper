7d6e3a19facc3451e71b41003922319d41baeaf6
myuanz <provefars@gmail.com>
2024-06-30 20:46:16 +0800

    添加resize相关逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,12 @@
 #pragma comment(lib, "Gdiplus.lib")
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-void CaptureAndDrawBitmap(HWND hwnd, int x, int y, int w, int h);
+void CaptureAndDrawBitmap(HWND hwnd, int w, int h);
 
 HBITMAP g_hBitmap = NULL;
-int g_captureX = 0;       // 截图起始X坐标
-int g_captureY = 0;       // 截图起始Y坐标
-int g_captureWidth = 666;   // 截图宽度
-int g_captureHeight = 888;  // 截图高度
+
+int g_captureWidth = 0;   // 截图宽度
+int g_captureHeight = 0;  // 截图高度
 bool g_isDragging = false;
 
 Gdiplus::SolidBrush* g_enable_brush;
@@ -37,7 +36,8 @@ Gdiplus::SolidBrush* g_disable_brush;
 
 HWND g_targetWindow = NULL;
 
-ImageType img = ImageType(0, 0);
+ImageType g_img = ImageType(pvz_size.height, pvz_size.width);
+auto g_screen_buffer = std::vector<uint8_t>();
 
 VOID InitConsole() {
   if (AttachConsole(ATTACH_PARENT_PROCESS)) {
@@ -93,9 +93,6 @@ void UpdateCaptureArea() {
   if (g_targetWindow) {
     RECT rect;
     GetClientRect(g_targetWindow, &rect);
-    g_captureX = rect.left;
-    g_captureY = rect.top;
-
     g_captureWidth = rect.right - rect.left;
     g_captureHeight = rect.bottom - rect.top;
     // fmt::print("Capture area: {} x {} at ({}, {})\n", g_captureWidth, g_captureHeight, g_captureX, g_captureY);
@@ -158,10 +155,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
   RegisterClass(&wc);
 
+  RECT rect = {0, 0, pvz_size.width, pvz_size.height};
+  AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
   HWND hwnd = CreateWindowEx(
     0, CLASS_NAME, CLASS_NAME, WS_OVERLAPPEDWINDOW, 
     CW_USEDEFAULT, CW_USEDEFAULT, 
-    500, 500, 
+    rect.right - rect.left, rect.bottom - rect.top, 
     NULL, NULL, hInstance, NULL
   );
 
@@ -187,12 +187,10 @@ LRESULT CALLBACK WindowProc(
 ) {
   switch (uMsg) {
     case WM_CREATE: {
-      SetTimer(hwnd, 1, 100, NULL);
-
       g_targetWindow = FindProcessWindow(L"PlantsVsZombies.exe");
       if (g_targetWindow) {
         UpdateCaptureArea();
-        SetTimer(hwnd, 1, 100, NULL);
+        SetTimer(hwnd, ID_SNAPSHOT_TIMER, 100, NULL);
         if (!RegisterHotKey(
           hwnd, ID_HOT_KEY, MOD_CONTROL | MOD_ALT, 'G'
         )) {
@@ -221,7 +219,7 @@ LRESULT CALLBACK WindowProc(
       UpdateCaptureArea();
       if (g_captureHeight > 0 && g_captureWidth > 0){
         CaptureAndDrawBitmap(
-          hwnd, g_captureX, g_captureY, 
+          hwnd,  
           g_captureWidth, g_captureHeight
         );
       }
@@ -281,7 +279,7 @@ LRESULT CALLBACK WindowProc(
   return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-void getBitmapBuffer(HBITMAP hBitmap, ImageType& output_img) {
+void getScaledBitmapBuffer(HDC hdc, HBITMAP hBitmap, ImageType& output_img) {
   BITMAP bmp;
   GetObject(hBitmap, sizeof(BITMAP), &bmp);
 
@@ -290,21 +288,59 @@ void getBitmapBuffer(HBITMAP hBitmap, ImageType& output_img) {
 
   BITMAPINFOHEADER bi = {0};
   bi.biSize = sizeof(BITMAPINFOHEADER);
-  bi.biWidth = width;
-  bi.biHeight = -height;  // Top-down DIB
+  bi.biWidth = pvz_size.width;
+  bi.biHeight = -pvz_size.height;  // Top-down DIB
   bi.biPlanes = 1;
   bi.biBitCount = 32;
   bi.biCompression = BI_RGB;
 
-  HDC hdc = GetDC(NULL);
-  GetDIBits(
-    hdc, hBitmap, 0, height, 
-    output_img.data.get(), (BITMAPINFO*)&bi, DIB_RGB_COLORS
-  );
-  ReleaseDC(NULL, hdc);
+  if (width != pvz_size.width || height != pvz_size.height) {
+    // 表明现在窗口不是标准大小, 应当获取图片后裁剪到标准大小再放到 output_img.data
+    int new_width = width, new_height = height;
+    int new_x_start = 0, new_y_start = 0;
+
+    if (1.0 * width / height > 1.0 * pvz_size.width / pvz_size.height) {
+      // 宽度过大, 应当裁剪左右
+      new_width = height * pvz_size.width / pvz_size.height;
+      new_x_start = (width - new_width) / 2;
+    } else if (1.0 * width / height < 1.0 * pvz_size.width / pvz_size.height) {
+      // 高度过大, 应当裁剪上下
+      new_height = width * pvz_size.height / pvz_size.width;
+      new_y_start = (height - new_height) / 2;
+    }
+    HDC hdcPartial = CreateCompatibleDC(NULL);
+    HBITMAP hBitmapPartial = CreateCompatibleBitmap(hdc, new_width, new_height);
+    SelectObject(hdcPartial, hBitmapPartial);
+    BitBlt(hdcPartial, 0, 0, new_width, new_height, hdc, new_x_start, new_y_start, SRCCOPY);
+
+    // 裁剪完成后再把 hBitmapPartial 缩放到标准大小
+    HDC hdcResize = CreateCompatibleDC(NULL);
+    HBITMAP hBitmapResize = CreateCompatibleBitmap(hdc, pvz_size.width, pvz_size.height);
+    SelectObject(hdcResize, hBitmapResize);
+    SetStretchBltMode(hdcResize, HALFTONE);
+    StretchBlt(
+      hdcResize, 0, 0, pvz_size.width, pvz_size.height, 
+      hdcPartial, 0, 0, new_width, new_height, SRCCOPY
+    );
+    GetDIBits(
+      hdcResize, hBitmapResize, 0, pvz_size.height, 
+      output_img.data.get(), (BITMAPINFO*)&bi, DIB_RGB_COLORS
+    );
+    
+    SelectObject(hdc, hBitmapResize);
+    DeleteObject(hBitmapResize);
+    DeleteDC(hdcPartial);
+    DeleteDC(hdcResize);
+  } else {
+    GetDIBits(
+      hdc, hBitmap, 0, height, 
+      output_img.data.get(), (BITMAPINFO*)&bi, DIB_RGB_COLORS
+    );
+  }
 }
 
-void CaptureAndDrawBitmap(HWND hwnd, int x, int y, int w, int h) {
+// 从屏幕截图并缩放到标准大小, 返回缩放后的 HDC, 图像保存在 g_img 中
+HDC CapturePVZAndResize() {
   HDC hdcScreen = GetDC(NULL);
   HDC hdcMemDC = CreateCompatibleDC(hdcScreen);
 
@@ -312,31 +348,25 @@ void CaptureAndDrawBitmap(HWND hwnd, int x, int y, int w, int h) {
     DeleteObject(g_hBitmap);
   }
 
-  g_hBitmap = CreateCompatibleBitmap(hdcScreen, w, h);
+  g_hBitmap = CreateCompatibleBitmap(hdcScreen, g_captureWidth, g_captureHeight);
   HBITMAP hbmOld = (HBITMAP)SelectObject(hdcMemDC, g_hBitmap);
 
   PrintWindow(g_targetWindow, hdcMemDC, PW_CLIENTONLY);
+  getScaledBitmapBuffer(hdcMemDC, g_hBitmap, g_img);
+  ReleaseDC(NULL, hdcScreen);
+  return hdcMemDC;
+}
 
-  if (img.rows != h || img.cols != w) {
-    img.resize(h, w);
-    
-    RECT rect = {0, 0, w, h};
-    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+void CaptureAndDrawBitmap(HWND hwnd, int w, int h) {
+  HDC hdcMemDC = CapturePVZAndResize();
 
-    int width = rect.right - rect.left;
-    int height = rect.bottom - rect.top;
-
-    SetWindowPos(hwnd, NULL, 0, 0, width, height, SWP_NOZORDER | SWP_NOMOVE);
-  }
-
-  getBitmapBuffer(g_hBitmap, img);
   // fmt::print("Image size: {} x {}\n", img.width(), img.height());
   RGBPix target_rgb = {110, 50, 19};
 
   auto col_sum = std::vector<int>(w, 0);
   for (int y = 0; y < h; ++y) {
     for (int x = 0; x < w; ++x) {
-      if (target_rgb == img(x, y)) {
+      if (target_rgb == g_img(x, y)) {
         col_sum[x]++;
         // fmt::print("eq {} {}\n", x, y);
       }
@@ -375,28 +405,23 @@ void CaptureAndDrawBitmap(HWND hwnd, int x, int y, int w, int h) {
   }
   peaks.erase(peaks.begin(), peaks.begin() + i);
 
-  int card_top = 8;
-  int card_width = 50;
-  int card_height = 70;
   Gdiplus::Graphics graphics(hdcMemDC);
   Card card;
 
   for (const auto peak : peaks) {
-    int x = peak - card_width;
-    int y = 0 + card_top;
-    card = Card(&img, x, y, card_width, card_height);
+    int x = peak - pvz_size.card_width;
+    int y = pvz_size.card_top;
+    card = Card(&g_img, x, y, pvz_size.card_width, pvz_size.card_height);
     auto enable = card.count();
 
+    auto draw_h = 15;
     graphics.FillRectangle(
-      // enable ? g_enable_brush : g_disable_brush, 
-      g_enable_brush, 
-      peak-card_width, 0+card_top+card_height, card_width, 30
+      enable ? g_enable_brush : g_disable_brush, 
+      peak-pvz_size.card_width, pvz_size.card_top+pvz_size.card_height-draw_h, 
+      pvz_size.card_width, draw_h
     );
   }
 
-  SelectObject(hdcMemDC, hbmOld);
   DeleteDC(hdcMemDC);
-  ReleaseDC(NULL, hdcScreen);
-
   InvalidateRect(hwnd, NULL, FALSE);
 }
