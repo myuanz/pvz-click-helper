f85e2f9da5642393b54cdae2e37e898d9b720dc5
myuanz <provefars@gmail.com>
2024-07-01 22:33:23 +0800

    一些阈值测试

--- test.py
+++ test.py
@@ -5,14 +5,18 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
-target_rgb = np.array((110, 50, 19))
-target_bgr = target_rgb[::-1]
+target_rgbs = np.array([
+    (110, 50, 19),
+    (104, 68, 55),
+])
+
+target_bgrs = target_rgbs[:, ::-1]
 
 # %%
-src_img_p = Path('pvz1.png')
+src_img_p = Path('pvz2.png')
 
 img = cv2.imread(str(src_img_p))
-mask = (img == target_bgr).all(axis=2)
+mask = (img == target_bgrs[0]).all(axis=2) | (img == target_bgrs[1]).all(axis=2)
 
 plt.imshow(mask)
 # %%
@@ -53,7 +57,7 @@ plt.plot(peaks[i:], col_sum[peaks[i:]], "x")
 # %%
 
 img = cv2.imread(str(src_img_p))
-mask = (img == target_bgr).all(axis=2)
+mask = (img == target_bgrs[0]).all(axis=2) | (img == target_bgrs[1]).all(axis=2)
 col_sum = mask.sum(0)
 
 peaks = []
@@ -72,4 +76,47 @@ for i in range(0, len(peaks)-windows_size):
     if delta.std() < 1 and delta.mean() > 20:
         break
 plt.plot(col_sum)
-plt.plot(peaks[i:], col_sum[peaks[i:]], "x")
+peaks = peaks[i-1:]
+plt.plot(peaks, col_sum[peaks], "x")
+
+# %%
+card_width = delta.mean()
+cards = []
+
+for peak1, peak2 in zip(peaks[:-1], peaks[1:]):
+    cards.append(img[8+28:78+28, peak1:peak2])
+    plt.imshow(cards[-1])
+    plt.title(cards[-1].std())
+    plt.show()
+    
+
+# %%
+target_top_rgb = np.array((81, 33, 11))
+plt.imshow((cards[0] == target_top_rgb[::-1]).all(axis=2)[:100, :])
+# %%
+# enable_card = cards[1]
+# disable_card = cards[2]
+# # plt.imshow(card)
+# plt.subplot(1, 2, 1)
+# plt.hist(enable_card.ravel(), bins=256, color='r', alpha=0.5)
+# plt.subplot(1, 2, 2)
+# plt.hist(disable_card.ravel(), bins=256, color='b', alpha=0.5)
+# %%
+fig, axes = plt.subplots(3, 4, figsize=(12, 9))
+axes = axes.ravel()
+
+for ax, card in zip(axes, cards):
+    ax.hist(card.ravel(), bins=128, color='r', alpha=0.5)
+    less_50 = (card < 50).sum()
+    more_200 = (card > 200).sum()
+    ax.set_title(f"{more_200:3d} / {less_50:3d} = {more_200 / less_50:.2f}")
+
+plt.show()
+fix, axes = plt.subplots(3, 4, figsize=(12, 9))
+axes = axes.ravel()
+
+for ax, card in zip(axes, cards):
+    ax.imshow(card)
+plt.show()
+
+# %%
